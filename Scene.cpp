e9f73598fc0b368cb570ba8ff736087993d695af
Shiv Sundram <shivsundram@berkeley.edu>
2014-10-26 00:05:23 -0700

    shadows, but ambient coefficient are hardcoded

--- Scene.cpp
+++ Scene.cpp
@@ -1,12 +1,13 @@
 // Scene.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 #include "lodepng.h"
 #include <vector>
 #include <cmath>
 #include <iostream>
-#include <Eigen/Core>
+#include<Eigen/StdVector>
+//#include <Eigen/Core>
 
 #include <Eigen/Dense>
 using namespace Eigen;
@@ -71,11 +72,22 @@ Film::Film(int width1, int height1){
 
 
  //GLOBAL VARIABLES. EYE AND FOCAL PLANE
+ /*
  Vector3f eye(0.0f, 0.0f, 0.0f); 
  Vector3f UL(-1.0f, 1.0f, -1.0f);
  Vector3f UR(1.0f, 1.0f, -1.0f);
  Vector3f LL(1.0f, -1.0f, -1.0f);
  Vector3f LR(-1.0f, -1.0f, -1.0f);
+ */
+
+ Vector3f eye(0.0f, 0.0f, 100.0f);
+ Vector3f LL(-50.0f, -50.0f, 0.0f);
+ Vector3f LR(50.0f, -50.0f, 0.0f);
+ Vector3f UL(-50.0f, 50.0f, 0.0f);
+ Vector3f UR(50.0f, 50.0f, 0.0f);
+
+
+
  Vector3f xvec; // horizontal basis vector of focal plane
  Vector3f yvec; // vertical basis vector of focal plane
 
@@ -95,9 +107,9 @@ Film::Film(int width1, int height1){
 	 t_max = max; 
  }
  
- Ray generateRay(float i, float j){
-	 float focalplane = (UL +(xvec * i / 100)+(yvec * j / 100))[2];
-	 Vector3f pixel_loc = Vector3f(UL[0]+xvec[0]*i/100,UL[0]+ yvec[1]*j/100,focalplane );
+ Ray generateRay(float i, float j, int width, int height){
+	 float focalplane = (UL +(xvec * i / width)+(yvec * j / height))[2];
+	 Vector3f pixel_loc = Vector3f(UL[0]+xvec[0]*i/width,UL[0]+ yvec[1]*j/height,focalplane );
 	 Vector3f direction = pixel_loc - eye;
 	 direction.normalize();
 	 ///FIX ME
@@ -123,7 +135,7 @@ Film::Film(int width1, int height1){
  }
 
  Color Color::operator +(Color param) {
-	 return Color(param.red + red, param.blue + blue, param.green + green);
+	 return Color(param.red + red, param.green + green, param.blue + blue);
  }
 
  //Class for directional lights
@@ -131,8 +143,8 @@ Film::Film(int width1, int height1){
  public:
 	 Vector3f direction;
 	 float red;
-	 float blue;
 	 float green;
+	 float blue;
  };
 
  //Class for point lights
@@ -140,22 +152,35 @@ Film::Film(int width1, int height1){
  public:
 	 Vector3f source;
 	 float red;
-	 float blue;
 	 float green;
+	 float blue;
 	 Plight(Vector3f src, float r, float g, float b);
  };
 
  Plight::Plight(Vector3f src, float r, float g, float b){
 	 source = src;
 	 red = r;
+	 green = g;
 	 blue = b;
-	 green = g; 
  }
 
+ class Alight{
+ public:
+	 float red;
+	 float green;
+	 float blue;
+	 Alight (float r, float g, float b);
+ };
+ Alight::Alight(float r, float g, float b){
+	 red = r;
+	 green = g;
+	 blue = b;
+ }
 
  vector <Plight> plights;
- vector <Dlight> dlights;
- 
+ vector <Alight> alights; 
+ //vector <Dlight> dlights;
+ //Eigen::aligned_allocator<Eigen::Vector4f>  ;
 
 
 //SCENE CLASS
@@ -177,6 +202,7 @@ Film::Film(int width1, int height1){
  public:
 	 Vector3f specularcoef;
 	 Vector3f diffusecoef; 
+	 Vector3f ambientcoef; 
 	 virtual int hit(Ray * r) = 0 ;
 	 virtual Color diffuse(Ray * r, float t)=0; 
 	 virtual Color specular(Ray * r, float t)=0; 
@@ -192,6 +218,7 @@ Film::Film(int width1, int height1){
 	 Color diffuse(Ray * r, float t);
 	 Color specular(Ray * r, float t);
 	 float intersect(Ray *r);
+	 Color diffspec(Ray *r, float t, int );
  };
 
 
@@ -229,104 +256,118 @@ Film::Film(int width1, int height1){
 	 float discr = term1 - term2;
 
 	 float t1 = (-(d.dot(ec)) - sqrt(discr)) / (d.dot(d));
-	// float t2 = (-(d.dot(ec)) + sqrt(discr)) / (d.dot(d));
+	 float t2 = (-(d.dot(ec)) + sqrt(discr)) / (d.dot(d));
 
-	 return t1;
+	 return min(t1,t2);
 
- }
-
- Color Sphere::diffuse(Ray * r, float t){
-	 Color rgb(0.0f, 0.0f, 0.0f); 
-	 for (int a = 0.0f; a < plights.size(); a++){
-		 
-		 Vector3f surfacepoint = r->source + t*(r->direction);
-		 Vector3f l = plights[a].source - surfacepoint;
-		 l.normalize();
-		 
-		 Vector3f n = surfacepoint - center;
-		 n.normalize();
-
-		 float result = n.dot(l);
-		 rgb.red += max(result, 0.0f)*diffusecoef[0]*plights[a].red;
-		 rgb.green += max(result, 0.0f)*diffusecoef[1]*plights[a].blue;
-		 rgb.blue += max(result, 0.0f)*diffusecoef[2]*plights[a].green;
-	 }
-	 //cout << "Dot product: " << rgb.red<< "blue" << rgb.blue<< endl;
-	 return rgb;
- }
-
- Color Sphere::specular(Ray * r, float t){
-
-	 Color rgb(0.0f, 0.0f, 0.0f);
-	 for (int a = 0.0f; a < plights.size(); a++){
-
-		 Vector3f surfacepoint = r->source + t*(r->direction);
-		 Vector3f l = plights[a].source - surfacepoint;
-		 l.normalize();
-
-		 Vector3f n = surfacepoint - center;
-		 n.normalize();
-
-		 Vector3f r = Vector3f(0.f, 0.0f, 0.0f)+l - (2 * (l.dot(n)))*(n);
-		// r.normalize(); 
-
-		 Vector3f v = surfacepoint-eye;
-		 v.normalize();
-
-		 float result = r.dot(v);
-
-		 float p = 2;
-
-		 if (result > 0.0f){
-			 float result2 = powf(result, p);
-			 rgb.red += result2*specularcoef[0]*plights[a].red;
-			 rgb.blue += result2*specularcoef[1]*plights[a].blue;
-			 rgb.green += result2*specularcoef[2]*plights[a].green;
-		 }
-
-		 /*
-		 rgb.red += pow(max(result, 0.0f), p)*specularcoef[0] * plights[a].red;
-		 rgb.green += pow(max(result, 0.0f), p)*specularcoef[1] * plights[a].blue;
-		 rgb.blue += pow(max(result, 0.0f), p)*specularcoef[2] * plights[a].green;
-		 */
-	 }
-	 //cout << "Dot product: " << rgb.red<< "blue" << rgb.blue<< endl;
-	 //return Color(0.0f, 0.0f, 0.0f);
-	 return rgb;
  }
 
  vector<Sphere> balls;
 
 
- Color trace1(Ray * ray, Sphere * sphere){
-	 float red = 200;
-	 float blue = 100;
-	 float green = 155;
-	 if (sphere->hit(ray)==1){
-		 float intersect = sphere->intersect(ray);
-		 return sphere->diffuse(ray, intersect) + sphere->specular(ray, intersect);
-		 //return Color(red, blue, green);
+ Color Sphere::diffspec(Ray * r, float t, int id){
+	 Color rgb(0.0f, 0.0f, 0.0f);
+	 
+	 
+	 for (int a = 0.0f; a < plights.size(); a++){
+
+		 Vector3f surfacepoint = r->source + t*(r->direction);
+		 Vector3f l = plights[a].source - surfacepoint;
+		 l.normalize();
+
+		 //shadows
+		 bool isShadowHit = false; 
+		 Ray shadow(surfacepoint, l, .001f, 9999999999.0f);
+		 for (int x = 0; x < balls.size(); x++){
+			 int hit = balls[x].hit(&shadow);
+			 float intersect = balls[x].intersect(&shadow);
+			 if ((hit == 1) && (x!=id) && (intersect < (r->t_max)) && (intersect > (r->t_min)) ){
+				 isShadowHit = true;
+				 x = balls.size();
+			 }
+		 }
+
+		 if (!isShadowHit){
+			 Vector3f n = surfacepoint - center;
+			 n.normalize();
+
+			 float result = n.dot(l);
+			 rgb.red += max(result, 0.0f)*diffusecoef[0] * plights[a].red;
+			 rgb.green += max(result, 0.0f)*diffusecoef[1] * plights[a].green;
+			 rgb.blue += max(result, 0.0f)*diffusecoef[2] * plights[a].blue;
+
+			 Vector3f v = -surfacepoint + eye;
+			 v.normalize();
+
+			 Vector3f d = l;
+
+			 Vector3f r1 = -d;
+			 float q = 2 * d.dot(n);
+			 Vector3f r2 = q*n;
+			 Vector3f r = r1 + r2;
+
+			 r.normalize();
+
+			 result = r.dot(v);
+
+			 float p = 25;
+
+			 float result2 = powf(max(result, 0.0f), p);
+			 rgb.red += result2*specularcoef[0] * plights[a].red;
+			 rgb.green += result2*specularcoef[1] * plights[a].green;
+			 rgb.blue += result2*specularcoef[2] * plights[a].blue;
+
+		 }
 	 }
-	 return Color(0.0f, 0.0f, 0.0f);
+	 return rgb; 
  }
 
- Color trace(Ray * ray, vector<Sphere>  balls){
+
+ Color trace(Ray * ray, vector<Sphere>  balls, int depth){
+
+	 if (depth == 0){
+		 return Color(0.0f, 0.0f, 0.0f);
+	 }
 
 	 float closest_t = 999999999999999999; //FIX ME MAX FLOAT
 	 Sphere * closest_sphere = 0; 
+	 bool hit = false;
+	 int id = 0; 
 	 for (int x = 0; x < balls.size(); x++){
 		 Sphere  sphere = ((balls)[x]);
 		 if (sphere.hit(ray) == 1){
+			 hit = true;
 			 float intersect = sphere.intersect(ray);
 			 if (intersect < closest_t){
 				 closest_t = intersect;
 				 closest_sphere = &((balls)[x]);
+				 id = x; 
 			 }
-		 return closest_sphere->diffuse(ray, closest_t) + closest_sphere->specular(ray, closest_t);
-		 //return Color(red, blue, green);
 		}
 	}
-	 return Color(0.0f, 0.0f, 0.0f);
+
+	 Color rgb(0.0f, 0.0f, 0.0f);
+
+
+	 if (hit){	
+		 //return closest_sphere->diffuse(ray, closest_t) + closest_sphere->specular(ray, closest_t)+trace(;
+		 /*
+		 for (int a = 0.0f; a < alights.size(); a++){
+			 rgb.red += .3f*alights[a].red;
+			 rgb.green += .15*alights[a].green;
+			 rgb.blue += 0.0f*alights[a].blue;
+		 }
+		 */
+
+		 for (int a = 0.0f; a < plights.size(); a++){
+			 rgb.red += .3f*plights[a].red;
+			 rgb.green += .15*plights[a].green;
+			 rgb.blue += 0.0f*plights[a].blue;
+		 }
+
+		 rgb= rgb + closest_sphere->diffspec(ray, closest_t, id);
+	 }
+	 return rgb;;
  }
 
 
@@ -361,29 +402,35 @@ Film::Film(int width1, int height1){
 
 
  int main(int argc, char *argv[]){
-	 int height = 200;
-	 int width = 200; 
+	 int height = 900;
+	 int width = 900; 
 
 	 xvec = UR - UL;
 	 yvec = UL - LL;
 
 
 	 Film negative(width, height);
-	 Vector3f rgbc(1.0f, 1.0f, 1.0f);
-	 Vector3f rgbd(0.0f, 0.4f, 0.3f);
-	 Sphere  test=Sphere(Vector3f(0.0f, .0f, -2.0f), 1.0f, rgbc, rgbd);
+	 //Sphere  test = Sphere(Vector3f(0.0f, -.5f, -2.0f), 1.0f, Vector3f(1.0f, 0.0f, 0.0f), Vector3f(.3f, 0.5f, 0.2f));
+	 Sphere test = Sphere(Vector3f(-30.0f, 0.0f, -50.0f), 25.0f, Vector3f(.8f, 0.4f, 0.0f), Vector3f(.8f, 0.4f, 0.0f));
 	 balls.push_back(test);
-	 Sphere  test2 = Sphere(Vector3f(1.0f, 0.0f, -2.5f), 1.0f, Vector3f(1.0f, 0.0f, 1.0f), Vector3f(0.3f, 0.5f, 0.2f));
+	 //Sphere  test2 = Sphere(Vector3f(1.0f, 0.0f, -2.5f), 1.0f, Vector3f(1.0f, 0.0f, 1.0f), Vector3f(.3f, 0.5f, 0.2f));
+	 Sphere test2 = Sphere(Vector3f(30.0f, 0.0f, -50.0f), 25.0f, Vector3f(.8f, 0.4f, 0.0f), Vector3f(.8f, 0.4f, 0.0f));
+	 
 	 balls.push_back(test2);
 
-	 Plight source(Vector3f(2.0f, 2.0f, 2.0f), 200.0f, 0.0f, 155);
+	 //Plight source(Vector3f(2.0f, 2.0f, 2.0f), 255.0f, 255.0f, 255.0f);
+	 Plight source(Vector3f(200.0f, 0.0f, -50.0f), 255.0f, 255.0f, 255.0f);
 	 plights.push_back(source);
+
+	 Alight ambient(51.0f, 51.0f, 51.0f);
+	 alights.push_back(ambient);
 
 	 for (int i = 0; i < width; i++){
 		 for (int j = 0; j < height; j++){
-			 Ray * temp = &generateRay(i, j);
-			 Color * result = &trace(temp, balls);
-			 negative.commit(i, j, result->red, result->green, result->blue);
+			 Ray * temp = &generateRay(i, j, width, height);
+			 Color * result = &trace(temp, balls, 1);
+			 negative.commit(i, j, min(result->red, 255.0f), min(result->green, 255.0f), min(result->blue, 255.0f));
+			// cout << "Color: (" << i << ", " << j << "): is (" << result->red << ", " << result->green << "," << result->blue << ")" << endl;
 			 //FIX ME: currently only supporting one sample per pixel
 			 //negative.commit(i, j, 255*i/width, 255*j/height, 200) activate this for pretty colors
 		 }
@@ -430,6 +477,13 @@ Film::Film(int width1, int height1){
 
 
 
+
+
+
+
+
+
+
 //Andrey, the following is SHIT WE DO NOT NEED. ALSO FROM LODEPNG WEBSITE. ALTERNATE WAYS OF ENCODING IMAGE
 
 //Example 2
@@ -459,4 +513,75 @@ void encodeWithState(const char* filename, std::vector<unsigned char>& image, un
 
 	//if there's an error, display it
 	if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+}
+
+
+
+Color trace1(Ray * ray, Sphere * sphere){
+	float red = 200;
+	float blue = 100;
+	float green = 155;
+	if (sphere->hit(ray) == 1){
+		float intersect = sphere->intersect(ray);
+		//return sphere->diffuse(ray, intersect) + sphere->specular(ray, intersect);
+		return sphere->diffspec(ray, intersect, 1);
+		//return Color(red, blue, green);
+	}
+	return Color(0.0f, 0.0f, 0.0f);
+}
+
+
+Color Sphere::diffuse(Ray * r, float t){
+	Color rgb(0.0f, 0.0f, 0.0f);
+	for (int a = 0.0f; a < plights.size(); a++){
+
+		Vector3f surfacepoint = r->source + t*(r->direction);
+		Vector3f l = plights[a].source - surfacepoint;
+		l.normalize();
+
+		Vector3f n = surfacepoint - center;
+		n.normalize();
+
+		float result = n.dot(l);
+		rgb.red += max(result, 0.0f)*diffusecoef[0] * plights[a].red;
+		rgb.green += max(result, 0.0f)*diffusecoef[1] * plights[a].green;
+		rgb.blue += max(result, 0.0f)*diffusecoef[2] * plights[a].blue;
+	}
+	return rgb;
+}
+
+Color Sphere::specular(Ray * r, float t){
+
+	Color rgb(0.0f, 0.0f, 0.0f);
+	for (int a = 0.0f; a < plights.size(); a++){
+
+		Vector3f surfacepoint = r->source + t*(r->direction);
+		Vector3f d = plights[a].source - surfacepoint;
+		d.normalize();
+
+		Vector3f n = surfacepoint - center;
+		n.normalize();
+
+
+		Vector3f v = -surfacepoint + eye;
+		v.normalize();
+
+		Vector3f r1 = -d;
+		float q = 2 * d.dot(n);
+		Vector3f r2 = q*n;
+		Vector3f r = r1 + r2;
+
+		r.normalize();
+
+		float result = r.dot(v);
+
+		float p = 2;
+
+
+		float result2 = powf(max(result, 0.0f), p);
+		rgb.red += result2*specularcoef[0] * plights[a].red;
+		rgb.green += result2*specularcoef[1] * plights[a].green;
+		rgb.blue += result2*specularcoef[2] * plights[a].blue;
+	}
+	return rgb;
 }
